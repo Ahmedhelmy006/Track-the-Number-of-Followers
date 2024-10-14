d1ef87ac28cee8abf51a9d87c4ae14562d99a26d
Ahmed Helmy <117530532+Ahmedhelmy006@users.noreply.github.com>
2024-10-14 17:11:39 +0300

    Update main.py

--- core/main.py
+++ core/main.py
@@ -1,14 +1,14 @@
-import asyncio
 from PlaywrightDriver import PlaywrightDriver
 from core import FollowersTracker
 import GoogleFormsSubmitter
+import time
 
-async def run_scraper(): 
+def run_scraper(): 
     driver_instance = PlaywrightDriver(cookies_file='json.json')
-    context = await driver_instance.initialize_driver()
-    
-    tracker = FollowersTracker(context, r'input files/Accounts.xlsx', r'input files/Pages.xlsx')
-    followers_data = await tracker.scrap_info()
+    context = driver_instance.initialize_driver()
+    tracker = FollowersTracker(context, r'input files\Accounts.xlsx', r'input files\Pages.xlsx')
+
+    followers_data = tracker.scrap_info()
 
     form_url = 'https://docs.google.com/forms/d/e/1FAIpQLSeK_A8x_7ipnICGwK3k3MdTq3vGhXwfu9BhSz37Bgz27T1llw/formResponse'
     
@@ -27,14 +27,9 @@ async def run_scraper():
     }
 
     form_submitter = GoogleFormsSubmitter.GoogleFormsSubmitter(form_url, form_fields)
-    await form_submitter.submit_data(mapped_data)
+    form_submitter.submit_data(mapped_data)
 
-    await driver_instance.close(context)
+    driver_instance.close(context)
 
-async def main():
-    while True:
-        await run_scraper()
-        await asyncio.sleep(6 * 60 * 60)  # Wait for 6 hours between each run
-
-if __name__ == "__main__":
-    asyncio.run(main())
+while True:
+    run_scraper()
