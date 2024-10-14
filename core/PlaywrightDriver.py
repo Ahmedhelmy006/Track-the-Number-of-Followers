94c648d28809b48cd4ba3d5195378702d69bfd41
Ahmed Helmy <117530532+Ahmedhelmy006@users.noreply.github.com>
2024-10-14 16:22:49 +0300

    Update PlaywrightDriver.py

--- core/PlaywrightDriver.py
+++ core/PlaywrightDriver.py
@@ -1,21 +1,21 @@
-from playwright.sync_api import sync_playwright
+from playwright.async_api import async_playwright
 import json
 
 class PlaywrightDriver:
     def __init__(self, cookies_file=None):
         self.cookies_file = cookies_file
 
-    def initialize_driver(self):
-        playwright = sync_playwright().start()
-        browser = playwright.chromium.launch(headless=True)
-        context = browser.new_context()
+    async def initialize_driver(self):
+        async with async_playwright() as playwright:
+            browser = await playwright.chromium.launch(headless=True)
+            context = await browser.new_context()
 
-        if self.cookies_file:
-            with open(self.cookies_file, 'r') as file:
-                cookies = json.load(file)
-                context.add_cookies(cookies)
+            if self.cookies_file:
+                with open(self.cookies_file, 'r') as file:
+                    cookies = json.load(file)
+                    await context.add_cookies(cookies)
 
-        return context
+            return context
 
-    def close(self, context):
-        context.close()
+    async def close(self, context):
+        await context.close()
