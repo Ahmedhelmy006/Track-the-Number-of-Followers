cbdedf99dd63d944016be8b8c4dbd84198e0fbe8
Ahmed Helmy <117530532+Ahmedhelmy006@users.noreply.github.com>
2024-10-31 23:03:29 +0300

    Add files via upload

---
+++ core/X.py
@@ -0,0 +1,4 @@
+class X:
+    followers_label_selector = '.YouTubeUserTopInfo span.YouTubeUserTopLight'
+    followers_value_selector = '.YouTubeUserTopInfo span[style*="font-weight: bold;"]'
+    last_30_days_selector = 'p:has(span)'
---
+++ core/Youtube.py
@@ -0,0 +1,5 @@
+class YouTube:
+    subscribers_label = 'Subscribers'
+    views_label = 'Views'
+    label_class = 'index__label--ooY4q'
+    value_class = 'index__value--3w6TU'
