ec904215b6c1c543072f3ddd6689b09ad8a580c8
Ahmed Helmy <117530532+Ahmedhelmy006@users.noreply.github.com>
2024-10-31 23:00:56 +0300

    Update main.py

--- core/main.py
+++ core/main.py
@@ -1,12 +1,21 @@
 from PlaywrightDriver import PlaywrightDriver
 from core import FollowersTracker
 import GoogleFormsSubmitter
-import time
+import time as t
 
-def run_scraper(): 
-    driver_instance = PlaywrightDriver(cookies_file='cookies.json')
-    context = driver_instance.initialize_driver()
-    tracker = FollowersTracker(context, r'input files/Accounts.xlsx', r'input files/Pages.xlsx')
+def run_scraper():
+    driver = PlaywrightDriver(cookies_file='cookies.json')
+    context = driver.initialize_driver()
+
+    tracker = FollowersTracker(
+        context,
+        r'input files//Accounts.xlsx',
+        r'input files//pages.xlsx',
+        r'/input files//Newstellers.xlsx',
+        r'/input files//Youtube.xlsx',
+        r'/Instagram.xlsx',
+        r'/input files//X.xlsx'  # Twitter (X) links
+    )
 
     followers_data = tracker.scrap_info()
 
@@ -16,17 +25,35 @@ def run_scraper():
         'Business Infographics': 'entry.627780398',
         'Nicolas Boucher Online': 'entry.1247179473',
         'AI Finance Club': 'entry.1150066733',
-        'Nicolas Boucher Personal Account': 'entry.269674828'
+        'Nicolas Boucher Personal Account': 'entry.269674828',
+        'AI + Finance by Nicolas Boucher Newsteller': 'entry.508828894',
+        'Nicolas Boucher Online Videos | YouTube Subscribers': 'entry.114799247',
+        'Nicolas Boucher Online Videos | YouTube Total Views': 'entry.1433952907',
+        'Instagram Total Followers': 'entry.147129590',
+        'X Total Number of Followers': 'entry.121881511',  # Twitter followers
+        'X Followers Last 30 Days': 'entry.1810313943',    # Twitter growth
+        'X Followers Last 30 Days Percentage': 'entry.1647920330'  # New growth percentage entry
     }
 
     mapped_data = {
         'Business Infographics': followers_data[0],
         'Nicolas Boucher Online': followers_data[1],
         'AI Finance Club': followers_data[2],
-        'Nicolas Boucher Personal Account': followers_data[3]
+        'Nicolas Boucher Personal Account': followers_data[3],
+        'AI + Finance by Nicolas Boucher Newsteller': followers_data[4],
+        'Nicolas Boucher Online Videos | YouTube Subscribers': followers_data[5]['subscribers'],
+        'Nicolas Boucher Online Videos | YouTube Total Views': followers_data[5]['views'],
+        'Instagram Total Followers': followers_data[6],
+        'X Total Number of Followers': followers_data[7]['followers'],  
+        'X Followers Last 30 Days': followers_data[7]['last_30_days_growth'],  
+        'X Followers Last 30 Days Percentage': followers_data[7]['Growth Percentege']  
     }
 
     form_submitter = GoogleFormsSubmitter.GoogleFormsSubmitter(form_url, form_fields)
     form_submitter.submit_data(mapped_data)
 
-run_scraper()
+    driver.close(context)
+
+if __name__ == "__main__":
+    run_scraper()
+        
