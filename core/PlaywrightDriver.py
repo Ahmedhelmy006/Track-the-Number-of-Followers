5e3198c301a4414560e653ea9723c23c45c6e5a6
Ahmed Helmy <117530532+Ahmedhelmy006@users.noreply.github.com>
2024-11-06 12:29:09 +0200

    Update PlaywrightDriver.py

--- core/PlaywrightDriver.py
+++ core/PlaywrightDriver.py
@@ -1,27 +1,26 @@
 from playwright.sync_api import sync_playwright
 import os
-import json
 
 class PlaywrightDriver:
-    def __init__(self, cookies_file=None, chrome_path="/usr/bin/google-chrome"):
-        self.cookies_file = cookies_file
+    def __init__(self, chrome_path="/usr/bin/google-chrome"):
+        # Path to the extracted profile directory
+        self.user_data_dir = "./chrome-profile"
         self.chrome_path = chrome_path
 
     def initialize_driver(self):
         playwright = sync_playwright().start()
 
         context = playwright.chromium.launch_persistent_context(
-            user_data_dir="./chrome-profile",
-            headless=False,
-            executable_path=self.chrome_path 
+            user_data_dir=self.user_data_dir,  
+            headless=False,                   
+            executable_path=self.chrome_path   
         )
 
-        if self.cookies_file and os.path.exists(self.cookies_file):
-            with open(self.cookies_file, 'r') as file:
-                cookies = json.load(file)
-                context.add_cookies(cookies)
+        print("Chrome launched with persistent profile data.")
 
         return context
 
     def close(self, context):
+        # Close the browser context, which saves any updates to the profile
         context.close()
+        print("Chrome context closed and session saved.")
